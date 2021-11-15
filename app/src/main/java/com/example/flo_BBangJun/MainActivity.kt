a075686adbd44e83ffff1b48fc1d7048f8729cd0
BYEONGJUN KANG <lucid9dawn@gmail.com>
2021-11-16 03:49:32 +0900

    7weeks second commit(11/16)

--- app/src/main/java/com/example/flo_BBangJun/MainActivity.kt
+++ app/src/main/java/com/example/flo_BBangJun/MainActivity.kt
@@ -3,75 +3,66 @@ package com.example.flo_BBangJun
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.flo_BBangJun.databinding.ActivityMainBinding
-import com.example.flo_BBangJun.SongActivity
 import com.google.gson.Gson
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
     lateinit var binding: ActivityMainBinding
-//    private lateinit var player: Player // 쓰레드 2: 전역변수
-//    private val handler = Handler(Looper.getMainLooper()) // 쓰레드 7: 메인쓰레드에 메시지를 보낼 것이기 때문에 Looper.getMainLooper()
-//    private var gson: Gson = Gson() // Gson
-//    private var song: Song = Song()
+    private lateinit var song: Song
+
+
+//    // 미디어 플레이어
 //    private var mediaPlayer: MediaPlayer? = null
+//
+//    // Gson
+//    private val gson: Gson = Gson()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         initNavigation()
+        inputDummySongs()
 
-        val song= Song("라일락", "아이유(IU)", 0, 214, false, "lilac")
-//        player=Player(song.playTime, song.isPlaying) // 쓰레드3: 쓰레드2에서의 lateinit으로 인해 선언.객체만 생성하면 쓰레드가 시작되는 것이 아님. 생성자값을 인자로 넣어주기
-//        player.start() // 쓰레드4: 스레드 시작
-
-
-        // 버튼 클릭 시 수행할 동작을 지정하는 리스너(함수)
         binding.mainPlayerLayout.setOnClickListener {
-            val intent = Intent(this, SongActivity::class.java)
-            intent.putExtra("title", song.title)
-            intent.putExtra("singer", song.singer)
-            intent.putExtra("second", song.second)
-            intent.putExtra("playTime", song.playTime)
-            intent.putExtra("isPlaying", song.isPlaying)
-            intent.putExtra("music", song.music)
-            startActivity(intent) // startActivity 메소드를 통해서 메인액티비티에서 송액티비티로 전환
-            }
-//        val music = resources.getIdentifier(song.music, "raw", this.packageName)
-//        mediaPlayer = MediaPlayer.create(this, music)
+            Log.d("nowSongId", song.id.toString())
+            val editor = getSharedPreferences("song", MODE_PRIVATE).edit()
+            editor.putInt("songId", song.id)
+            editor.apply()
 
-//        binding.mainMiniplayerBtn.setOnClickListener {
-//            if(player.state==Thread.State.TERMINATED){ // 쓰레드의 재활용을 위함. (무한재생)
-//                player=Player(song.playTime, song.isPlaying)
-//                player.start()
-//            }
-//            player.isPlaying = true
-//            song.isPlaying = true
-//            songPlayPause(true)
-//            mediaPlayer?.start()
-//            val intent=Intent(this, SongActivity::class.java)
-//            intent.putExtra("play", false)
-//        }
-//
-//        binding.mainMiniplayerpauseIB.setOnClickListener {
-//            player.isPlaying=false
-//            song.isPlaying = false
-//            songPlayPause(false)
-//            mediaPlayer?.pause()
-//            val intent=Intent(this, SongActivity::class.java)
-//            intent.putExtra("play", false)
-//        }
+            val intent = Intent(this@MainActivity, SongActivity::class.java)
 
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val spf = getSharedPreferences("song", MODE_PRIVATE)
+        val songId = spf.getInt("songId", 0)
+
+        val songDB = SongDatabase.getInstance(this)!!
+        song = if (songId == 0) {
+            songDB.songDao().getSong(1)
+        } else {
+            songDB.songDao().getSong(songId)
+        }
+
+        Log.d("song ID", song.id.toString())
+        setMiniPlayer(song)
+    }
+
+    private fun initNavigation() {
+        supportFragmentManager.beginTransaction().replace(R.id.main_frm, HomeFragment())
+            .commitAllowingStateLoss()
 
         binding.mainBnv.setOnItemSelectedListener {
             when (it.itemId) {
@@ -106,94 +97,192 @@ class MainActivity : AppCompatActivity() {
             }
             false
         }
-
     }
 
-//    fun songPlayPause(isplaying : Boolean){ // 클릭했을 때 재생-정지 전환해주는 함수
-//        if(isplaying){
-//            binding.mainMiniplayerBtn.visibility = View.GONE
-//            binding.mainMiniplayerpauseIB.visibility = View.VISIBLE
-//        }
-//        else{
-//            binding.mainMiniplayerBtn.visibility = View.VISIBLE
-//            binding.mainMiniplayerpauseIB.visibility = View.GONE
-//        }
-//    }
-//
-//    private fun setMiniPlayer(song: Song){
-//        binding.mainMiniplayertitleTV.text = song.title
-//        binding.mainMiniplayersingerTV.text = song.singer
-//        binding.songPlayerSB.progress=(song.second*1000/song.playTime)
-//
-//        if(song.isPlaying){
-//            binding.mainMiniplayerpauseIB.visibility = View.VISIBLE
-//            binding.mainMiniplayerBtn.visibility = View.GONE
-//        } else{
-//            binding.mainMiniplayerpauseIB.visibility = View.GONE
-//            binding.mainMiniplayerBtn.visibility = View.VISIBLE
-//        }
-//    }
-//
-//    inner class Player(private val playTime:Int, var isPlaying: Boolean): Thread(){
-//        private var second=0
-//        override fun run(){
-//            try {
-//                while (true) {
-//                    if (second >= playTime) {
-//                        handler.post{
-//                            second=0
-//                            binding.songPlayerSB.progress=0
-//                            binding.mainMiniplayerpauseIB.visibility=View.GONE
-//                            binding.mainMiniplayerBtn.visibility=View.VISIBLE
-//                        }
-//                        break
-//                    }
-//
-//                    if (isPlaying) {
-//                        sleep(1000)
-//                        second++
-//                        handler.post {
-//                            binding.songPlayerSB.progress = second * 1000 / playTime
-//                        }
-//                    }
-//                }
-//            }catch(e:InterruptedException){
-//                Log.d("intterupt", "쓰레드 종료!")
-//            }
-//        }
-//    }
+    private fun setMiniPlayer(song: Song) {
+        binding.mainMiniplayerTitleTv.text = song.title
+        binding.mainMiniplayerSingerTv.text = song.singer
+        binding.mainProgressSb.progress = (song.second * 1000 / song.playTime)
 
-    private fun initNavigation() {
-        supportFragmentManager.beginTransaction().replace(R.id.main_frm, HomeFragment())
-            .commitAllowingStateLoss()
+        val music = resources.getIdentifier(song.music, "raw", this.packageName)
+
+//        mediaPlayer = MediaPlayer.create(this, music)
+
+        if (song.isPlaying) {
+            binding.mainMiniplayerPauseIv.visibility = View.VISIBLE
+            binding.mainMiniplayerPlayIv.visibility = View.GONE
+        } else {
+            binding.mainMiniplayerPauseIv.visibility = View.GONE
+            binding.mainMiniplayerPlayIv.visibility = View.VISIBLE
+        }
     }
 
-//    override fun onPause() { // 생명주기에 의한 opPause 상태
-//        super.onPause()
-//        mediaPlayer?.pause() // 미디어플레이어 중지
-//        player.isPlaying = false // 스레드 중지
-//        song.isPlaying = false // 노래 중지 상태
-//        song.second = (binding.songPlayerSB.progress*song.playTime)/1000
-//    songPlayPause(false) // 정지 상태의 이미지로 전환
-//}
-//
-//override fun onDestroy() {
-//    super.onDestroy()
-//    player.interrupt()
-//    mediaPlayer?.release() // mediaPlayer가 가지고 있던 리소스(라일락) 해제
-//    mediaPlayer = null // mediaPlayer 해제
-//}
-//
-//    override fun onStart() {
-//        super.onStart()
-//        val sharedPreferences = getSharedPreferences("song", MODE_PRIVATE) // mode_private: 이 앱에서만 sharedpreferences에 접근 가능
-//        // 값을 단순히 가져오는 것이기 때문에 sharedpreferences를 조작하지 않기에 editor가 필요 없음
-//        val jsonSong = sharedPreferences.getString("song", null) // songactivity에서의 editor에서 song으로 가져왔기 때문
-//        song = if(jsonSong == null){
-//            Song("라일락", "아이유(IU)", 0, 214, false, "lilac")
-//        } else{
-//            gson.fromJson(jsonSong, Song::class.java)
-//        }
-//        setMiniPlayer(song)
-//    }
+    //ROOM_DB
+    private fun inputDummySongs() {
+        val songDB = SongDatabase.getInstance(this)!!
+        val songs = songDB.songDao().getSongs()
+
+        if (songs.isNotEmpty()) return
+
+        songDB.songDao().insert(
+            Song(
+                "Lilac",
+                "아이유 (IU)",
+                0,
+                200,
+                false,
+                "music_lilac",
+                R.drawable.img_album_exp2,
+                false
+            )
+        )
+
+        songDB.songDao().insert(
+            Song(
+                "Flu",
+                "아이유 (IU)",
+                0,
+                200,
+                false,
+                "music_lilac",
+                R.drawable.img_album_exp2,
+                false
+            )
+        )
+
+        songDB.songDao().insert(
+            Song(
+                "Butter",
+                "방탄소년단 (BTS)",
+                0,
+                190,
+                false,
+                "music_lilac",
+                R.drawable.img_album_exp,
+                false
+            )
+        )
+
+        songDB.songDao().insert(
+            Song(
+                "Butter (Hotter Remix)",
+                "방탄소년단 (BTS)",
+                0,
+                190,
+                false,
+                "music_lilac",
+                R.drawable.img_album_exp,
+                false
+            )
+        )
+
+        songDB.songDao().insert(
+            Song(
+                "Butter (Sweeter Remix)",
+                "방탄소년단 (BTS)",
+                0,
+                190,
+                false,
+                "music_lilac",
+                R.drawable.img_album_exp,
+                false
+            )
+        )
+
+        songDB.songDao().insert(
+            Song(
+                "Next Level",
+                "에스파 (AESPA)",
+                0,
+                210,
+                false,
+                "music_lilac",
+                R.drawable.img_album_exp3,
+                false
+            )
+        )
+
+        songDB.songDao().insert(
+            Song(
+                "Next Level (IMLAY Remix)",
+                "에스파 (AESPA)",
+                0,
+                210,
+                false,
+                "music_lilac",
+                R.drawable.img_album_exp3,
+                false
+            )
+        )
+
+        songDB.songDao().insert(
+            Song(
+                "Boy with Luv",
+                "방탄소년단 (BTS)",
+                0,
+                230,
+                false,
+                "music_lilac",
+                R.drawable.img_album_exp4,
+                false
+            )
+        )
+
+        songDB.songDao().insert(
+            Song(
+                "소우주 (Mikrokosmos)",
+                "방탄소년단 (BTS)",
+                0,
+                230,
+                false,
+                "music_lilac",
+                R.drawable.img_album_exp4,
+                false
+            )
+        )
+
+        songDB.songDao().insert(
+            Song(
+                "Make It Right",
+                "방탄소년단 (BTS)",
+                0,
+                230,
+                false,
+                "music_lilac",
+                R.drawable.img_album_exp4,
+                false
+            )
+        )
+
+        songDB.songDao().insert(
+            Song(
+                "BBoom BBoom",
+                "모모랜드 (MOMOLAND)",
+                0,
+                240,
+                false,
+                "music_lilac",
+                R.drawable.img_album_exp5,
+                false
+            )
+        )
+
+        songDB.songDao().insert(
+            Song(
+                "궁금해",
+                "모모랜드 (MOMOLAND)",
+                0,
+                240,
+                false,
+                "music_lilac",
+                R.drawable.img_album_exp5,
+                false
+            )
+        )
+
+
+        val _songs = songDB.songDao().getSongs()
+        Log.d("DB DATA", _songs.toString())
+
+    }
 }
--- app/src/main/java/com/example/flo_BBangJun/Song.kt
+++ app/src/main/java/com/example/flo_BBangJun/Song.kt
@@ -1,5 +1,9 @@
 package com.example.flo_BBangJun
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "SongTable")
 // 제목, 가수, 사진, 재생시간, 현재 재생시간, isplaying(재생 되고 있는지)
 data class Song(
         var title : String ="", // 변수: 노래 제목
@@ -7,6 +11,10 @@ data class Song(
         var second : Int = 0, // 변수: 재생시간이 현재 몇 초인지
         var playTime : Int = 0, // 총재생시간 200=3분20초
         var isPlaying : Boolean = false, // 재생되고 있는 지
-        var music : String = "" // 변수: 어떤 음악이 재생되고 있었는지
-)
+        var music : String = "", // 변수: 어떤 음악이 재생되고 있었는지
+        var coverImg: Int? = null, // 커버이미지
+        var isLike: Boolean = false, // 좋아요 버튼 클릭 유무 판단
+) {
+        @PrimaryKey(autoGenerate = true) var id: Int=0 // autogenerate를 통해 id값을 자동으로 생성
+}
 
